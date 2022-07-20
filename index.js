85a5f9de232f3de573f482388eb5bbb2d9be92d0
HoneyTwix <chocolateenchanted@gmail.com>
2022-07-20 10:31:49 -0500

    emply

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const db = require('./connection');
 //     if (err) throw err
 // });
 
-function beginTracker() {
+function startTracker() {
     inquirer.prompt([
         {
             type: 'list',
@@ -32,7 +32,7 @@ function beginTracker() {
             ],
         }
     ]).then(function(val) {
-        // use a switch to prompt certain choices
+        // use a switch to prompt a choice
         switch (val.choose) {
             case 'View departments':
                 viewDepts();
@@ -65,178 +65,4 @@ function beginTracker() {
     })
 }
 
-// view functions
-
-function viewDepts() {
-    db.query('SELECT * from department', (err, res) => {
-        if (err) throw (err);
-        console.table(res);
-        beginTracker();
-    })
-};
-
-function viewRoles() {
-    db.query('SELECT * from roles', (err, res) => {
-        if (err) throw (err);
-        console.table(res);
-        beginTracker();
-    })
-};
-
-function viewEmployees() {
-    db.query('SELECT * from employee', (err, res) => {
-        if (err) throw (err);
-        console.table(res);
-        beginTracker();
-    })
-};
-
-// add functions
-function addDept() {
-    const deptQuestions = [
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What department would you like to add?'
-        }
-    ]
-
-    inquirer.prompt(deptQuestions).then((answers) => {
-        const info = `INSERT INTO department (name) VALUES('${answers.title}')`;
-        db.query(info, (err) => {
-            if (err) {
-                console.log(err);
-            }
-        });
-    });
-};
-
-function addRole() {
-    const roleQuestions = [
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What role would tou like to add?'
-        },
-
-        {
-            type: 'input',
-            name: 'salary',
-            message: 'What is their salary?'
-        },
-
-        {
-            type: 'input',
-            name: 'deptId',
-            message: 'What is the department ID for this role?'
-        }
-    ]
-
-    inquirer.prompt(roleQuestions).then((answers) => {
-        const info = `INSERT INTO roles (title, salary, department_id) VALUES('${answers.title}', '${answers.salary}', '${answers.deptId}')`;
-        db.query(info, (err) => {
-            if (err) {
-                console.log(err);
-            }
-        });
-    });
-};
-
-function addEmployee() {
-    const employeeQuestions = [
-        {
-            type: 'input',
-            name: 'firstname',
-            message: 'What is the first name of this employee?'
-        },
-
-        {
-            type: 'input',
-            name: 'lastname',
-            message: 'What is the last name of this employee?'
-        },
-
-        {
-            type: 'input',
-            name: 'roleId',
-            message: 'What is the employee role ID?'
-        },
-
-        {
-            type: 'input',
-            name: 'managerId',
-            message: 'What is the manager ID of this employee, if they are a manager?'
-        }
-    ]
-
-    inquirer.prompt(employeeQuestions).then((answers) => {
-        const info = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES('${answers.firstname}', '${answers.lastname}', '${answers.roleId}', '${answers.managerId}')`;
-        db.query(info, (err) => {
-            if (err) {
-                console.log(err);
-            }
-        });
-    });
-};
-
-// update employee
-function updateEmployee() {
-    const staff = `SELECT * FROM employee`;
-    db.query(staff, (err, result) => {
-        if (err) {
-            console.log(err);
-        }
-
-        const staffNames = result.map(
-            (employee) => `${employee.first_name} ${employee.last_name}`
-        );
-        const updateStaff = [
-            {
-                type: 'list',
-                name: 'employee',
-                message: 'What profile would you like to edit?',
-                choices: staffNames,
-            }
-        ]
-
-        inquirer.prompt(updateStaff).then((empAnsw) => {
-            const info = `SELECT * FROM roles`;
-            db.query(info, (err, result) => {
-                if (err) {
-                    console.log(err);
-                }
-                const roleId = result.map(
-                    (roles) => `${roles.id}`
-                );
-
-                const updateRoleQuestions = [
-                    {
-                        type: 'list',
-                        name: 'roles',
-                        message: 'Which role do you want to update to?',
-                        choices: roleId
-                    }
-                ]
-                inquirer.prompt(updateRoleQuestions).then((roleAnsw) =>{
-                // const info = `INSERT INTO employee(role_id) VALUES(${answers.roles})`
-                const info = `Update employee Set role_id = ${roleAnsw} where employee_name = ${empAnsw})`
-
-                db.query(info, (err) => {
-                    if (err) {
-                        console.log(err);
-                    }
-                })});
-            });
-        });
-        
-    })   
-};
-beginTracker();
-
-// myFunc()
-//     .then(()=>{
-
-//     })
-//     .then(()=>{
-
-//     })
+startTracker()
